34661de972cf85906cf25dcfd54054ca630233aa
Daniel Iancu <dani.iancu@yahoo.com>
2025-10-10 20:40:12 +0100

    v1.0 - updated

--- pages/cancel.js
+++ pages/cancel.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Cancel() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>❌ Payment Cancelled</h1>
       <p>No worries — you can try again anytime!</p>
-      <a href="/" style={{ color: "#635bff" }}>← Back to Home</a>
+      <Link href="/" style={{ color: "#635bff" }}>← Back to Home</Link>
     </div>
   );
 }
--- pages/success.js
+++ pages/success.js
@@ -1,14 +1,14 @@
 import { 
   AiOutlineCheck
 } from "react-icons/ai";
-
+import Link from "next/link";
 
 export default function Success() {
   return (
     <div style={{ textAlign: "center", paddingTop:"100px", background:"grey", color:"white" }} >
       <h1><AiOutlineCheck style={{ background:"green", width:"50px", height:"50px", borderRadius:"100%", verticalAlign:"bottom" }} /> Payment Successful!</h1>
       <p>Thank you for your purchase. Your order will be processed shortly.</p>
-      <a href="/" style={{ color: "white" }}>← Back to Home</a>
+      <Link href="/" style={{ color: "white" }}>← Back to Home</Link>
     </div>
   );
 }
